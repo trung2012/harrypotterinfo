557de23bc093c2e103284d9e57f691d5576c1dc6
Trung Pham <trungpham2012@gmail.com>
2019-08-05 18:28:30 -0500

    fixed welcome page overflow

--- src/pages/welcome.component.js
+++ src/pages/welcome.component.js
@@ -4,8 +4,10 @@ import './welcome.styles.scss';
 
 const WelcomePage = ({ history }) => {
   return (
-    <div className='welcome-page'>
-      <div onClick={() => history.push('/home')} className="go-to-homepage">Get started</div>
+    <div className='welcome-page-container'>
+      <div className='welcome-page'>
+        <div onClick={() => history.push('/home')} className="go-to-homepage">Get started</div>
+      </div>
     </div>
   );
 }
