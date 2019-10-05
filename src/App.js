96521475c0c6b493743ad7ea28dcddc50fb30542
Trung Pham <trungpham2012@gmail.com>
2019-10-05 12:17:12 -0500

    added redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import WelcomePage from './pages/welcome.component';
